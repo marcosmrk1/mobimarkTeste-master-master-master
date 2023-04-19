71c19a4b8338d4f8bef98116faf69bdacae15e10
marcosmrk1 <marcospl.2990@gmail.com>
2023-04-19 13:20:31 -0300

    feat: TexTField são exibidos na tabela e a o select de turnos também

--- src/TelaListagem/TelaFormulario.js
+++ src/TelaListagem/TelaFormulario.js
@@ -1,13 +1,11 @@
-
 import React, { useEffect, useState } from "react";
 import {
     Box, TextField, Margin, Container, Typography, Checkbox, Select,
     ListItemText, FormControl, MenuItem, InputLabel, OutlinedInput, Card
 }
     from '@mui/material';
+import { Table, TableBody, TableCell, TableHead, TableRow } from "@material-ui/core";
 
-
-// CONST para a criação do select de Turnos e Localização da escola
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
 const MenuProps = {
@@ -23,40 +21,46 @@ const listaTurnos = [
     'Tarde',
     'Noite',
 ];
-
-// const names = [
-//     'Oliver Hansen',
-//     'Van Henry',
-//     'April Tucker',
-//     'Ralph Hubbard',
-//     'Omar Alexander',
-//     'Carlos Abbott',
-//     'Miriam Wagner',
-//     'Bradley Wilkerson',
-//     'Virginia Andrews',
-//     'Kelly Snyder',
-// ];
-
 const TelaFormulario = () => {
-    const [turnosUseState, setturnosUseState] = React.useState([]);
-    const [localizacaoDaEscola, setLocalizacaoDaEscola] = React.useState('');
-
-    const handleChange = (event) => {
+    const [valueParaOSelect, setValueParaOSelecet] = useState([]);
+    
+    const [localizacaoDaEscola, setLocalizacaoDaEscola] = useState('');
+    const [selectTunos,setselectTunos] = useState([])
+    const [ListaDeObjetosDosInputs, setListaDeObjetosDosInputs] = useState({
+        nomeDaEscola: '',
+        nomeDoDiretor: '',
+        localizacaoDaEscola: '',
+        turnos: '',
+    })
+    const [SelecionadoTurnosNoSelect,setSelecionadoTurnosNoSelect] = useState([])
+    const selectTurnos = (event) => {
         const {
             target: { value },
         } = event;
-        setturnosUseState(
+        setValueParaOSelecet(
             typeof value === 'string' ? value.split(',') : value,
         );
+        setSelecionadoTurnosNoSelect(value)
     };
 
-    const mudancaEvento = (event) => {
+    const selectLocalizacaoDaEscola = (event) => {
         setLocalizacaoDaEscola(event.target.value);
     };
-
+    const respostasDosTextFieldNomeEscolaEDiretor = (event) =>{
+        const{name,value} = event.target
+        setListaDeObjetosDosInputs({...ListaDeObjetosDosInputs,[name]:value})
+    }
+    const GetDoSelectTurnos =  (event) => {
+        const {
+            target: { value },
+        } = event;
+        setValueParaOSelecet( typeof value === 'string' ? value.join(',') : value,  
+        )
+        setSelecionadoTurnosNoSelect(value)
+    }
 
     // Fim da const de criação do select de Turnos e localização das escolas
-
+    
     return (
 
         <Card sx={{}}>
@@ -69,8 +73,12 @@ const TelaFormulario = () => {
                 >
                     <Typography component="h3" variant="h2" > Formulário</Typography>
                     <Typography component='h3' variant='h8'>Preencha corretamente seus Dados</Typography>
-                    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-                    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                    <TextField id="outlined-basic" label="Outlined" variant="outlined" 
+                     name="nomeDaEscola" value={ListaDeObjetosDosInputs.nomeDaEscola} onChange={respostasDosTextFieldNomeEscolaEDiretor}
+                     />
+                    <TextField id="outlined-basic" label="Outlined" variant="outlined"  
+                    name="nomeDoDiretor" value={ListaDeObjetosDosInputs.nomeDoDiretor} onChange={respostasDosTextFieldNomeEscolaEDiretor}
+                    />
 
                     <FormControl sx={{ m: 1, width: 300 }}>
                         <InputLabel id="demo-multiple-checkbox-label">Tag</InputLabel>
@@ -78,15 +86,16 @@ const TelaFormulario = () => {
                             labelId="demo-multiple-checkbox-label"
                             id="demo-multiple-checkbox"
                             multiple
-                            value={turnosUseState}
-                            onChange={handleChange}
+                            value={valueParaOSelect}
+                            onChange={selectTurnos}
                             input={<OutlinedInput label="Turnos" />}
                             renderValue={(selected) => selected.join(',')}
                             MenuProps={MenuProps}
+                          
                         >
                             {listaTurnos.map((turnosMapeados) => (
                                 <MenuItem key={turnosMapeados} value={turnosMapeados}>
-                                    <Checkbox checked={turnosUseState.indexOf(turnosMapeados) > -1} />
+                                    <Checkbox checked={valueParaOSelect.indexOf(turnosMapeados) > -1} />
                                     <ListItemText primary={turnosMapeados} />
                                 </MenuItem>
                             ))}
@@ -99,12 +108,38 @@ const TelaFormulario = () => {
                             id="demo-simple-select"
                             value={localizacaoDaEscola}
                             label="localizacaoDaEscola"
-                            onChange={mudancaEvento}
+                            onChange={selectLocalizacaoDaEscola}
                         >
                             <MenuItem value={1}>Urbana</MenuItem>
-                            <MenuItem value={2}>Rural</MenuItem>
+                            <MenuItem value={2}>Rural</MenuItem>                            
                         </Select>
                     </FormControl>
+                    <Card>
+                    <Container>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align='left'>Nome da escola</TableCell>
+                                    <TableCell align= 'left'>Nome do diretor</TableCell>
+                                    <TableCell align= 'left'>Turnos</TableCell>
+                                </TableRow>
+                                  
+                            </TableHead>
+                            <TableBody>
+                             <TableRow>
+                                    <TableCell>{ListaDeObjetosDosInputs.nomeDaEscola}</TableCell>
+                                    <TableCell>{ListaDeObjetosDosInputs.nomeDoDiretor}</TableCell>
+                                    {SelecionadoTurnosNoSelect.map((selecionadoTUrnos) =>(
+                                         <TableRow key={selecionadoTUrnos}>
+                                         <TableCell>{selecionadoTUrnos}</TableCell>
+                                         </TableRow>   
+                                         ))}
+                                </TableRow>
+                            </TableBody>
+                        </Table>  
+                        </Container>
+                       </Card>
+
                 </Box>
             </Container>
         </Card >
